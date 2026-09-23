f918cad26383e8c40644892c624aeed1f0da2ba3
Yash Dadlani <dadlaniyash2004@gmail.com>
2026-09-23 13:15:58 +0530

    update

--- database.py
+++ database.py
@@ -1,8 +1,12 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
-db_url="postgresql://postgres:Qwerty098@localhost:5432/prod"
-engine= create_engine(db_url)
+load_dotenv()
 
-session = sessionmaker(autocommit=False, autoflush=False, bind= engine)
+db_url = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@localhost:5432/prod")
+engine = create_engine(db_url)
+
+session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
